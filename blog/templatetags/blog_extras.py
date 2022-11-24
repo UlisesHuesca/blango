52caab8d5fb89bc9bc92e61a15b5fc82b2ce30fb
Ruperto Guiérrez <al02901351@tecmilenio.mx>
2022-11-24 18:07:39 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,9 +1,16 @@
 from django.contrib.auth import get_user_model
 from django import template
+from blog.models import Post
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 user_model = get_user_model()
 register = template.Library()
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk = post.pk)[:5]
+  return {"title":"Recent Posts","posts":posts}
+
 
 @register.filter
 def author_details(author, current_user):
@@ -29,3 +36,18 @@ def author_details(author, current_user):
   
   return format_html('{}{}{}',prefix, name, suffix)
 
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html("</div>")
